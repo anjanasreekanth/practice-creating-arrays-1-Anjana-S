3f71bd7a0f2517ec15c08c59ee168bbc6b997f97
Anjana Sreekanth <anjanasreekanth@outlook.com>
2026-05-01 14:04:48 -0500

    Practice Arrays1

---
+++ practice-arrays1.js
@@ -0,0 +1,20 @@
+
+//creating array for groceries  and numbers
+
+let groceryList = ["Carrot", "Apple","Onion","Tomato","Pears"];
+console.log(groceryList);
+
+let itemPrice = [36, 36, 87];
+console.log(itemPrice);
+
+//code to print 3 items from grocerList and 2 items from itemPrice
+
+for(let i = 0; i <3 ;i++){
+    console.log(groceryList[i]);
+    }
+    
+    
+for(let i = 0; i <2 ;i++){
+    console.log(itemPrice[i]);
+}
+    
